65d5dae787d3841aeadd8b973298c89b2b243a8c
ShifanaShajahan <84078184+ShifanaShajahan@users.noreply.github.com>
2021-05-19 18:07:04 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -2,16 +2,19 @@
 #include <stdio.h>
 #include <cs50.h>
 
-
+void draw_pyramid(void);
 int main(void) {
+  draw_pyramid();
+
+   void draw_pyramid(void) {
       int a= get_int("Enter type of pattern(1 or 2)");
       int b= get_int("Number of rows");
     
-    for(a=1)
+    if(a=1)
       int i,j;
-            for(i=5;i>=1;i--)
+            for(i=1;i<=5;i++)
              {  
-               for(j=1;j<=i;j++)
+               for(j=1;j<=5;j++)
                 {
                   printf("#");
                 }
@@ -19,5 +22,6 @@ int main(void) {
               }
           }
 
-     
+     }
+
     
