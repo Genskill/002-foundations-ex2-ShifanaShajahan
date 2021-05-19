c8c822ea892adf64b96e77175be98ab6f1eafe20
ShifanaShajahan <84078184+ShifanaShajahan@users.noreply.github.com>
2021-05-19 21:13:06 +0530

    Update pythagorean.c

--- pythagorean.c
+++ pythagorean.c
@@ -1,24 +1,19 @@
 //pythagorean.c
 #include<stdio.h>
 #include<cs50.h>
-#include<math.h>
-int main()
-{
-    int a, b;
-    float c;
 
-    //calculate the another side using Pythagoras Theorem
-    //a*a + b*b = c*c
-    //c = sqrt(a*a+b*b)
-    //maximum length should be equal to 30
-    for(a=1;a<=5;a++)
-    {
-        for(b=1;b<=5;b++)
-        {
-            c = sqrt(a*a+b*b);
-            if(c == (int)c)
-            {
-                printf("(%d, %d, %d)\n",a,b,(int)c);
-            }
-        }
+int main(void) {
+     int a= get_int("side");
+     int b= get_int("side");
+     int c= get_int("side");
+    
+    
+   if((a*a)=((b*b)+(c*c)))
+      { 
+       printf("Yes");
+      }
+   else  
+      {
+       printf("No");
+      }
     }
