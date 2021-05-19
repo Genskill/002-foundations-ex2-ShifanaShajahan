7b43bd38251cdc4a0647c9633212d321b52bf982
ShifanaShajahan <84078184+ShifanaShajahan@users.noreply.github.com>
2021-05-19 22:33:05 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -17,17 +17,7 @@ int main(void) {
         }
     
 
-       for(int i=1;i<=5;i++)
-       {
-         for(int j=1;j<=5;j++)
-          {
-            if(j>=5-i)
-               printf("#");
-            else 
-               printf(" ");
-          }
-            printf("\n");
-        }
+       
     } 
 
     
