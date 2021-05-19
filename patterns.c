070b3e302efa0ac6f636c198c27cda8da32efa9d
ShifanaShajahan <84078184+ShifanaShajahan@users.noreply.github.com>
2021-05-19 11:50:53 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -4,12 +4,32 @@
 
 
 int main(void) {
-  int height= get_int("Enter height");
-  
-  for(int row=5; row>=height; row--) {
-    for (int col=1; col<=height; col++){
-      printf("#");
-    }
-    printf("\n");
-  }
-}
+      int a= get_int("Enter type of pattern(1 or 2)");
+      int b= get_int("Number of rows");
+    
+    if(pattern 1) {
+            for(i=5;i>=1;i--)
+             {  
+               for(j=1;j<=i;j++)
+                {
+                  printf("#");
+                }
+                  printf("\n");
+              }
+          
+     else
+         for(i=1;i<=5;i++)
+              {
+                for(j=5-1;j>=i;j--)
+                  {
+                    printf(" ");
+                  }
+                     for(k=1;k<=i;k++)
+                         {
+                           printf("#");
+                         }
+                           printf("\n");
+                 }
+             }
+      }
+    
