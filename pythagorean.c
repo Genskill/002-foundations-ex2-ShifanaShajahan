c03f14ab84b36a3ab6806787eb8161195918f7fe
ShifanaShajahan <84078184+ShifanaShajahan@users.noreply.github.com>
2021-05-19 21:57:58 +0530

    Update pythagorean.c

--- pythagorean.c
+++ pythagorean.c
@@ -6,9 +6,9 @@ int main(void) {
      int a= get_int("side");
      int b= get_int("side");
      int c= get_int("side");
-    
-    
-   if((a*a)=((b*b)+(c*c)))
+
+     
+    if(a+b>=c;b+c>=a;a+c>=b;)
       { 
        printf("Yes");
       }
