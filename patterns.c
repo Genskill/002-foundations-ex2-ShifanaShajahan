aa91d93c1cafc78bee55f2df3448e815f1608ead
ShifanaShajahan <84078184+ShifanaShajahan@users.noreply.github.com>
2021-05-19 22:35:47 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -7,9 +7,9 @@ int main(void) {
         int b= get_int("Number of rows");
 
    int i,j;
-     for(int i=1;i<=5;i++)
+     for(int i=5;i>=1;i--)
        {
-         for(int j=1;j<=5;j++)
+         for(int j=1;j<=i;j++)
           {
             printf("#");
           }
