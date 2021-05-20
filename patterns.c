c8b29e2cfa5b9a26af7882df4837d9da0ae4952b
ShifanaShajahan <84078184+ShifanaShajahan@users.noreply.github.com>
2021-05-20 20:26:08 +0530

    Create patterns.c

--- patterns.c
+++ patterns.c
@@ -6,14 +6,10 @@ int main(void) {
 int a=get_int("Enter type of pattern (1 or 2)\n");
 int b=get_int("Number of rows");
   
-int i,j,k;
+int i,j;
 for(i=1;i<=4;i++)
   {
-  for(j=3;j>=i;j--)
-      {
-        printf(" ");
-      }
-    for(k=1;k<=i;k++)
+  for(j=1;j<=i;j++)
       {
       printf("#");
       {
