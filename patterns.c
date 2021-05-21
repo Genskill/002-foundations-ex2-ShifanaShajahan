3b060a72019e4b5f72c26ba7aa0b155d5f02e8c1
ShifanaShajahan <84078184+ShifanaShajahan@users.noreply.github.com>
2021-05-21 20:34:14 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -5,7 +5,7 @@
       
 int main(void){
       int a=get_int("Enter type of pattern (1 or 2)");
-      int b=get_int("Number of rows");
+      int b=get_int("Number of rows\n");
       printf("\n");
                 
  int i,j;
