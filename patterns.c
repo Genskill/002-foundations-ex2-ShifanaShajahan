8a8ccefa91c17b808c7ada493f520c1df9ffa987
ShifanaShajahan <84078184+ShifanaShajahan@users.noreply.github.com>
2021-05-21 13:14:03 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -2,17 +2,21 @@
 #include <stdio.h>
 #include <cs50.h>
   
+void draw_line(int num) {
+  int i;
+    for(i=0; i<num;i++) {
+      printf("#");
+      }
+        printf("\n");
+  }
+      
 int main(void){
       int a=get_int("Enter type of pattern (1 or 2)");
       int b= get_int("Number of rows\n");
       
-     int i,j;
-     for(i=4;i>0; i--)
+     int i;
+     for(i=10;i>0; i--)
           {
-           for(j=1;j<=i; j++)
-                {
-                 printf("#");
-                }
-          printf("\n");
-          }
+           draw_line(i);    
      }
+}
