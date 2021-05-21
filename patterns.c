9c3af15d361f810c0b93cb307a06f8834cedee2c
ShifanaShajahan <84078184+ShifanaShajahan@users.noreply.github.com>
2021-05-21 15:10:51 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -11,7 +11,7 @@ void draw_line(int num) {
   }
       
 int main(void){
-      int a=get_int("Enter type of pattern (1 or 2);
+      int a=get_int("Enter type of pattern (1 or 2)");
       int b=get_int("Number of rows");
       
      int i;
