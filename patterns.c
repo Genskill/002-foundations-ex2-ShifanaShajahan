6b32b75775b128d5a7eeaf1e05a920714080e202
ShifanaShajahan <84078184+ShifanaShajahan@users.noreply.github.com>
2021-05-21 13:16:50 +0530

    Create patterns.c

--- patterns.c
+++ patterns.c
@@ -1,1 +1,22 @@
-
+//patterns.c
+#include <stdio.h>
+#include <cs50.h>
+  
+void draw_line(int num) {
+  int i;
+    for(i=0; i<num;i++) {
+      printf("#");
+      }
+        printf("\n");
+  }
+      
+int main(void){
+      int a=get_int("Enter type of pattern (1 or 2)");
+      int b= get_int("Number of rows\n");
+      
+     int i;
+     for(i=4;i>0; i--)
+          {
+           draw_line(i);    
+     }
+}
