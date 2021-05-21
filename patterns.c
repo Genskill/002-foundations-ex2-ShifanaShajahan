56476082e005a11e2b14b5a5bea319054964a67a
ShifanaShajahan <84078184+ShifanaShajahan@users.noreply.github.com>
2021-05-21 12:34:35 +0530

    Create patterns.c

--- patterns.c
+++ patterns.c
@@ -1,3 +1,18 @@
 //patterns.c
-
-import subprocessimport sys def get_output(pattern, rows): p = subprocess.Popen("./patterns", stdout=subprocess.PIPE, stdin=subprocess.PIPE) op,_ = p.communicate(f"{pattern}\n{rows}\n".encode("ascii")) op = op.decode("ascii").replace("Enter type of pattern (1 or 2) Number of rows", "") return op def test(): expected = """##########""" obtained = get_output(1, 4) assert expected == obtained, f"Pattern 1, 4 rows\nExpected\n{expected} Got\n{obtained}" expected = """ # ## #######""" obtained = get_output(2, 4) assert expected == obtained, f"Pattern 2, 4 rows\nExpected\n{expected} Got\n{obtained}" expected = """###############""" obtained = get_output(1, 5) assert expected == obtained, f"Pattern 1, 5 rows\nExpected\n{expected} Got\n{obtained}" expected = """ # ## ### #########""" obtained = get_output(2, 5) assert expected == obtained, f"Pattern 2, 5 rows\nExpected\n{expected} Got\n{obtained}" return True if __name__ == "__main__": if test(): print ("All good") sys.exit(0)
+#include <stdio.h>
+#include <cs50.h>
+  
+int main(void){
+      int a=get_int("Enter type of pattern (1 or 2)\n");
+      int b= get_int("Nuber of rows\n");
+      
+     int i,j;
+     for(i=4;i>=1; i--)
+          {
+           for(j=1;j<=i; j++)
+                {
+                 printf("#");
+                }
+          printf("\n");
+          }
+     }
