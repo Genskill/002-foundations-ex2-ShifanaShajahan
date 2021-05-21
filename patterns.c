d9d320f783ebe757a5f71e972eef8ad886fe6ac5
ShifanaShajahan <84078184+ShifanaShajahan@users.noreply.github.com>
2021-05-21 21:11:25 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -7,7 +7,7 @@ int main(void){
       int c=get_int("Pattern 1, 4 rows");
       printf("\n");            
  int i,j;
-     for(i=4;i>=1;i--){
+     for(i=4;i>=0;i--){
        for(j=1;j<=i;j++){
          printf("#");}
          if(i==1)break;
