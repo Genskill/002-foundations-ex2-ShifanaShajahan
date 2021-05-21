20b4ca9e93231a22cf77219a992019d521345648
ShifanaShajahan <84078184+ShifanaShajahan@users.noreply.github.com>
2021-05-21 15:13:54 +0530

    Create patterns.c

--- patterns.c
+++ patterns.c
@@ -13,7 +13,7 @@ void draw_line(int num) {
 int main(void){
       int a=get_int("Enter type of pattern (1 or 2)");
       int b=get_int("Number of rows");
-      
+      printf("\n");
      int i;
      for(i=4;i>=1;i--)
           {
