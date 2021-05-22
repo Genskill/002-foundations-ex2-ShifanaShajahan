354aeaccd16409831e4e2813591649bc75cd06dc
ShifanaShajahan <84078184+ShifanaShajahan@users.noreply.github.com>
2021-05-22 09:47:43 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -1,15 +1,12 @@
 //patterns.c
 #include <stdio.h>
 #include <cs50.h> 
-int main(void){
-      
-      
-              
+int main(void){      
  int i,j;
      for(i=4;i>=1;i--){
        for(j=1;j<=i;j++){
          printf("#");}
-         if(i<=1)break;
+         if(i==1)break;
        printf("\n");
        }
   }
