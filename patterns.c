0f415cdd62c892d17b31c093e23d653c19314934
ShifanaShajahan <84078184+ShifanaShajahan@users.noreply.github.com>
2021-05-22 13:43:10 +0530

    Create patterns.c

--- patterns.c
+++ patterns.c
@@ -5,12 +5,12 @@ void draw_line(int num){
  int i;
   for(i=0; i<num; i++) {
    printf("#");}
- if(i==1) break;
  printf("\n"); }
 
 int main(void){      
  int i; 
- for(i=4; i>0; i++){
+ for(i=4; i>0; i++)
+   if(i==1) break; {
      draw_line(i);
        }
 }
