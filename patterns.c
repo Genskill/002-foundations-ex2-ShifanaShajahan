3e1e496aab842377c5a3928a09bffebd916d76b3
ShifanaShajahan <84078184+ShifanaShajahan@users.noreply.github.com>
2021-05-22 13:39:17 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -5,12 +5,12 @@ void draw_line(int num){
  int i;
   for(i=0; i<num; i++) {
    printf("#");}
- if(i==1)break;
+ if(i==1) break;
  printf("\n"); }
 
 int main(void){      
  int i; 
  for(i=4; i>0; i++){
-     draw_line(i)
+     draw_line(i);
        }
 }
